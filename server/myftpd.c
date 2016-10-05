288684fb4f2e0d26531f27651222905ac8a912d5
Jenna Wilson <jwilso27@nd.edu>
2016-10-05 00:44:57 -0400

    added .h file for functions

--- server/myftpd.c
+++ server/myftpd.c
@@ -3,27 +3,11 @@
 // Programming Assignment 3
 // 10/12/16
 
-#include <stdio.h>   
-#include <stdlib.h>  
-#include <unistd.h>
-#include <string.h>  
-#include <sys/types.h>   
-#include <sys/socket.h>  
-#include <sys/time.h>
-#include <sys/stat.h>
-#include <bits/stat.h>
-#include <time.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>   
-#include <netdb.h>   
-#include <dirent.h>
-#include <openssl/md5.h>
+#include "../program3.h"
 
 #define MAX_PENDING 5   
 #define MAX_LINE 4096 
 
-void my_send( int, char*, int );
-void my_recv( int, void*, int );
 void query( int, char* );
 int req_size( char* );
 void req_md5( int, char* );
@@ -130,60 +114,6 @@ int main(int argc, char *argv[]) {
             } else if ( strncmp( buf, "XIT", 3 ) == 0 ) close( new_s ); //exit
         }
     }  
-}
-
-void my_send( int s, char* buf, int flag ) {
-    short int len = strlen(buf);
-    int bufsize = 0;
-    char tmp_buf[16];
-
-    // send command length to server
-    if ( send( s, &len, sizeof(len), flag ) == -1 ) {    
-        perror("server send error");
-        exit(1);
-    }
-
-    // send command to server
-    while ( bufsize < len ) {
-        bzero( tmp_buf, sizeof(tmp_buf) );
-        strncpy( tmp_buf, &buf[bufsize], 15 );
-        printf("%s\n",tmp_buf);
-        if ( send( s, tmp_buf, sizeof(tmp_buf), flag ) == -1 ) {    
-            perror("server send error");
-            exit(1);
-        }
-        bufsize += 15;
-        printf("bufsize: %d\n",bufsize);
-    }
-    printf("buf: %s\n",buf);
-}
-
-void my_recv( int s, void* buf, int flag ) {
-    int tmp_len, bufsize = 0;
-    short int len;
-    char tmp_buf[16];
-
-    // receive string length from client
-    if ( recv( s, &len, sizeof(len), flag ) == -1 ) {
-        perror("server receive error");
-        exit(1);
-    }
-    printf("len: %d\n",len);
-    len = ntohs(len);
-    printf("len: %d\n",len);
-
-    // receive string from client
-    bzero( buf, sizeof(buf) );
-    while ( bufsize < len ) {
-        bzero( tmp_buf, sizeof(tmp_buf) );
-        if ( ( tmp_len = recv( s, tmp_buf, sizeof(tmp_buf), flag ) ) == -1 ) {
-            perror("server receive error");
-            exit(1);
-        } else bufsize += tmp_len;
-        strcat( buf, tmp_buf );
-        printf("bufsize: %d\n",bufsize);
-    }
-    printf("buf: %s\n",buf);
 }
 
 // int query( int s, char* buf ) {
